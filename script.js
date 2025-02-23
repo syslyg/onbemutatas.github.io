a69f469deb2f8e4d097b7255b1557b9ce9138098
SirDna63 <ritz.andras63@gmail.com>
2025-02-23 20:01:18 +0100

    joleszez

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ document.addEventListener("DOMContentLoaded", function() {
 document.getElementById("home-button").addEventListener("click", function () {
     location.reload();
 });
+function openImage(imgElement) {
+    const overlay = document.getElementById("image-overlay");
+    const overlayImg = document.getElementById("overlay-img");
+
+    overlayImg.src = imgElement.src;  // Az eredeti kép forrását átvesszük
+    overlay.style.display = "flex";  // Megjelenítjük az overlay-t
+}
+
+function closeImage() {
+    document.getElementById("image-overlay").style.display = "none";
+}
